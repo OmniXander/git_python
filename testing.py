0d328c06cde15680f0f03a118a070f69dbbc6a41
OmniXander <alejandroperdiguero@outlook.com>
2021-04-01 14:02:26 +0200

    Comiteo del bueno

---
+++ testing.py
@@ -0,0 +1,13 @@
+'''
+echo "# git_python" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:OmniXander/git_python.git
+git push -u origin main
+'''
+
+
+
+print("python me gusta menos que la pizza")
